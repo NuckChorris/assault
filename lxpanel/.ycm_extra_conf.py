7ad26719edff1d097de919d559f2482689e8d3d4
Zarek Jenkinson <aki@aki.pw>
2013-06-18 02:41:21 +1200

    [lxpanel] Added lxpanel libs to YCM conf and made it use GTK2

--- lxpanel/.ycm_extra_conf.py
+++ lxpanel/.ycm_extra_conf.py
@@ -10,8 +10,10 @@ flags = [
   '-pthread',
 ]
 
-flags += [s.strip() for s in subprocess.check_output(['pkg-config', '--cflags', 'gtk+-3.0']).strip().split(' ')]
-flags += [s.strip() for s in subprocess.check_output(['pkg-config', '--libs', 'gtk+-3.0']).strip().split(' ')]
+flags += [s.strip() for s in subprocess.check_output(['pkg-config', '--cflags', 'gtk+-2.0']).strip().split(' ')]
+flags += [s.strip() for s in subprocess.check_output(['pkg-config', '--cflags', 'lxpanel']).strip().split(' ')]
+flags += [s.strip() for s in subprocess.check_output(['pkg-config', '--libs', 'gtk+-2.0']).strip().split(' ')]
+flags += [s.strip() for s in subprocess.check_output(['pkg-config', '--libs', 'lxpanel']).strip().split(' ')]
 
 def FlagsForFile( filename ):
   return {
