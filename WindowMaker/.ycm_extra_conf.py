65c672d8b00a4caca9f50fe948092be0ee2ef6c4
Zarek Jenkinson <aki@aki.pw>
2013-06-18 00:54:54 +1200

    [WindowMaker] Made YCM conf better

--- WindowMaker/.ycm_extra_conf.py
+++ WindowMaker/.ycm_extra_conf.py
@@ -1,41 +1,17 @@
+import subprocess
+
 flags = [
-'-Wall',
-'-Wextra',
-'-Werror',
-'-fexceptions',
-'-DNDEBUG',
-'-DUSE_CLANG_COMPLETER',
-'-pthread',
-'-I/usr/include/gtk-3.0',
-'-I/usr/include/at-spi2-atk/2.0',
-'-I/usr/include/gtk-3.0',
-'-I/usr/include/gio-unix-2.0/',
-'-I/usr/include/cairo',
-'-I/usr/include/pango-1.0',
-'-I/usr/include/harfbuzz',
-'-I/usr/include/pango-1.0',
-'-I/usr/include/atk-1.0',
-'-I/usr/include/cairo',
-'-I/usr/include/pixman-1',
-'-I/usr/include/freetype2',
-'-I/usr/include/libdrm',
-'-I/usr/include/libpng16',
-'-I/usr/include/gdk-pixbuf-2.0',
-'-I/usr/include/libpng16',
-'-I/usr/include/glib-2.0',
-'-I/usr/lib/glib-2.0/include',
-'-lgtk-3',
-'-lgdk-3',
-'-lpangocairo-1.0',
-'-lpango-1.0',
-'-latk-1.0',
-'-lcairo-gobject',
-'-lcairo',
-'-lgdk_pixbuf-2.0',
-'-lgio-2.0',
-'-lgobject-2.0',
-'-lglib-2.0'
+  '-Wall',
+  '-Wextra',
+  '-Werror',
+  '-fexceptions',
+  '-DNDEBUG',
+  '-DUSE_CLANG_COMPLETER',
+  '-pthread',
 ]
+
+flags += [s.strip() for s in subprocess.check_output(['pkg-config', '--cflags', 'gtk+-3.0']).strip().split(' ')]
+flags += [s.strip() for s in subprocess.check_output(['pkg-config', '--libs', 'gtk+-3.0']).strip().split(' ')]
 
 def FlagsForFile( filename ):
   return {
