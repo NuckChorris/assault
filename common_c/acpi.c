db2a89fbc627476f3e363f575325f2a87d986993
Peter Lejeck <peter.lejeck@gmail.com>
2013-07-07 00:21:46 -0700

    [common_c] Convert to pure C99, removing dependency on GTK

--- common_c/acpi.c
+++ common_c/acpi.c
@@ -1,42 +1,45 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include "acpi.h"
 
-gboolean acpi_get_property
-(gchar *out, gchar *battery_id, gchar *property)
+bool acpi_get_property
+(char *out, char *battery_id, char *property)
 {
 	FILE *fp;
-	gchar filename[64];
+	char filename[64];
+
 	snprintf(filename, 64, ACPI_PATH, battery_id, property);
 	fp = fopen(filename, "r");
 	if (fp != NULL && fgets(out, 64, fp)) {
 		fclose(fp);
-		return TRUE;
+		return true;
 	}
-	return FALSE;
+	return false;
 }
 
-gboolean acpi_get_charging
-(gchar *battery_id)
+bool acpi_get_charging
+(char *battery_id)
 {
-	gchar value[64];
+	char value[64];
+
 	if (acpi_get_property(value, battery_id, "status")) {
 		if (strcmp(value, "Charging") == 0) {
-			return TRUE;
+			return true;
 		} else {
-			return FALSE;
+			return false;
 		}
 	} else {
-		return FALSE;
+		return false;
 	}
 }
 
-gdouble acpi_get_percent
-(gchar *battery_id)
+double acpi_get_percent
+(char *battery_id)
 {
-	gchar val_full[64], val_now[64];
-	guint full;
-	guint now;
+	char val_full[64], val_now[64];
+	unsigned int full;
+	unsigned int now;
 
 	if (acpi_get_property(val_full, battery_id, "charge_full")
 	 && sscanf(val_full, "%d", &full) == 1) {
@@ -54,7 +57,6 @@ gdouble acpi_get_percent
 		}
 	}
 	
-	// If we're here, we don't have anything, so return 0
 	return 0.0;
 }
 
--- common_c/acpi.h
+++ common_c/acpi.h
@@ -1,7 +1,6 @@
-#include <gtk/gtk.h>
-
+#include <stdbool.h>
 #define ACPI_PATH "/sys/class/power_supply/%s/%s"
 
-gboolean acpi_get_property (gchar *out, gchar *battery_id, gchar *property);
-gboolean acpi_get_charging (gchar *battery_id);
-gdouble acpi_get_percent (gchar *battery_id);
+bool acpi_get_property (char *out, char *battery_id, char *property);
+bool acpi_get_charging (char *battery_id);
+double acpi_get_percent (char *battery_id);
