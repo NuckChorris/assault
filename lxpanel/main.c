ef4a45a94120f5c26eaba8a7444e8c40bd40dd91
Peter Lejeck <peter.lejeck@gmail.com>
2014-03-24 16:23:48 -0700

    Fix bug based on rounding error or something
    I wanna level with you man: I have no idea if this is gonna do fucking
    ANYTHING.  I just took a crazy wild guess at the issue.

--- lxpanel/main.c
+++ lxpanel/main.c
@@ -51,7 +51,7 @@ gboolean draw_callback
 	#ifdef BACKGROUND_FILL_ENTIRE_WIDGET
 	cairo_rectangle(cr, 0, 0, width, height);
 	#else
-	cairo_rectangle(cr, (MARGIN / 2) + STROKE_WIDTH, (MARGIN / 2) + STROKE_WIDTH, BATTERY_WIDTH, BATTERY_HEIGHT);
+	cairo_rectangle(cr, (MARGIN / 2), (MARGIN / 2), BATTERY_WIDTH + STROKE_WIDTH*2, BATTERY_HEIGHT + STROKE_WIDTH*2);
 	#endif
 	cairo_set_source_rgba(cr, COLOR_BACKGROUND, OPACITY_BACKGROUND);
 	cairo_fill(cr);
